78f60f84943280a4febb7d0be4d693c792c02662
Veronika Lezhneva <veronikalezhneva@MacBook-Air-Veronika.local>
2024-02-07 13:51:12 +0300

    additional comments

--- conftest.py
+++ conftest.py
@@ -6,7 +6,7 @@ from selenium.webdriver.firefox.options import Options as OptionsFirefox
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default="chrome", help="Choose browser: chrome or firefox")
     parser.addoption('--language', action='store', default="en", help="Choose language: en/ru/es...")
-
+    #default Chrome and En
 @pytest.fixture(scope="function")
 def browser(request):
     browser_name = request.config.getoption("browser_name")

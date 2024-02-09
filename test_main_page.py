a736aa783c5c92c10a91418ff3915d27241520c9
Veronika Lezhneva <veronikalezhneva@MacBook-Air-Veronika.local>
2024-02-09 14:52:51 +0300

    добавлен переход к логин странице

--- test_main_page.py
+++ test_main_page.py
@@ -10,9 +10,11 @@ def test_guest_should_see_login_link(browser):
     page.should_be_login_link()
 
 def test_guest_can_go_to_login_page(browser):
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-    page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 def test_guest_check_login_link(browser):
     page = LoginPage(browser,linkLogin)

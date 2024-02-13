d1138399ae806242a460e15265bac9728cef9b6f
Veronika Lezhneva <veronikalezhneva@MacBook-Air-Veronika.local>
2024-02-13 13:54:27 +0300

    Тест перехода на логин страницу перенесены в Base_page

--- pages/product_page.py
+++ pages/product_page.py
@@ -16,3 +16,10 @@ class ProductPage(BasePage):
 		product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
 		basket_price = self.browser.find_element(*ProductPageLocators.BASKET_PRICE).text
 		assert product_price == basket_price, f"{product_price} not equal {basket_price}"
+
+	def should_not_be_success_message(self):
+		assert self.is_not_element_present(*ProductPageLocators.PRODUCT_IN_BASKET_ALERT), \
+       		"Success message is presented, but should not be"
+	def should_disappeared(self):
+		assert self.is_disappeared(*ProductPageLocators.PRODUCT_IN_BASKET_ALERT), \
+       		"Success message is not disappeared, but should be"
--- test_product_page.py
+++ test_product_page.py
@@ -1,12 +1,42 @@
 from pages.product_page import ProductPage
 import pytest
+Link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-@pytest.mark.parametrize('promo_offer', ["0","1","2", "3", "4", "5", "6",pytest.param("7", marks=pytest.mark.xfail(reason="Bug in WIP")), "8", "9"])
-def test_guest_can_add_product_to_basket(browser, promo_offer):
-    ProductLink = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
-    page = ProductPage(browser, ProductLink)
+#@pytest.mark.parametrize('promo_offer', ["0","1","2", "3", "4", "5", "6",pytest.param("7", marks=pytest.mark.xfail(reason="Bug in WIP")), "8", "9"])
+#def test_guest_can_add_product_to_basket(browser, promo_offer):
+    #ProductLink = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
+    #page = ProductPage(browser, ProductLink)
+    #page.open()
+    #page.add_product_to_basket()
+    #page.solve_quiz_and_get_code()
+    #page.should_be_product_is_in_basket()
+    #page.should_be_equal_price()
+
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, Link)
     page.open()
     page.add_product_to_basket()
-    page.solve_quiz_and_get_code()
-    page.should_be_product_is_in_basket()
-    page.should_be_equal_price()
+    page.should_not_be_success_message()
+
+def test_guest_cant_see_success_message(browser):
+    page = ProductPage(browser, Link)
+    page.open()
+    page.should_not_be_success_message()
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, Link)
+    page.open()
+    page.add_product_to_basket()
+    page.should_disappeared()
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    page = ProductPage(browser, Link)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    page = ProductPage(browser, Link)
+    page.open()
+    page.go_to_login_page()

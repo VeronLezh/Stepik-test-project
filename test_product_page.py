7fca139dd435fbf4e6691f91c526fd7d743f6b3f
Veronika Lezhneva <veronikalezhneva@MacBook-Air-Veronika.local>
2024-02-13 18:19:05 +0300

    Добавлены маркеры need review

--- test_product_page.py
+++ test_product_page.py
@@ -6,20 +6,21 @@ import faker
 
 Link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-#@pytest.mark.parametrize('promo_offer', ["0","1","2", "3", "4", "5", "6",pytest.param("7", marks=pytest.mark.xfail(reason="Bug in WIP")), "8", "9"])
-    #def test_guest_can_add_product_to_basket(browser, promo_offer):
-        #ProductLink = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
-        #page = ProductPage(browser, ProductLink)
-        #page.open()
-        #page.add_product_to_basket()
-        #page.solve_quiz_and_get_code()
-        #page.should_be_product_is_in_basket()
-        #page.should_be_equal_price()
+@pytest.mark.need_review
+@pytest.mark.parametrize('promo_offer', ["0","1","2", "3", "4", "5", "6",pytest.param("7", marks=pytest.mark.xfail(reason="Bug in WIP")), "8", "9"])
+def test_guest_can_add_product_to_basket(browser, promo_offer):
+    ProductLink = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
+    page = ProductPage(browser, ProductLink)
+    page.open()
+    page.add_product_to_basket()
+    page.solve_quiz_and_get_code()
+    page.should_be_product_is_in_basket()
+    page.should_be_equal_price()
 
 def test_guest_cant_see_success_message(browser):
-        page = ProductPage(browser, Link)
-        page.open()
-        page.should_not_be_success_message()
+    page = ProductPage(browser, Link)
+    page.open()
+    page.should_not_be_success_message()
 
 @pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
@@ -40,11 +41,13 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     page = ProductPage(browser, Link)
     page.open()
     page.go_to_login_page()
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page = ProductPage(browser, Link)
     page.open()
@@ -69,7 +72,8 @@ class TestUserAddToBasketFromProductPage():
         product_page = ProductPage(browser, Link)
         product_page.open()
         product_page.should_not_be_success_message()
-        
+    
+    @pytest.mark.need_review    
     def test_user_can_add_product_to_basket(self,browser,setup):
         product_page = ProductPage(browser, Link)
         product_page.open()
